e97d1e119310943ed09f051248722fb3e554583f
Namrata <124352728+Namsrkive@users.noreply.github.com>
2024-11-13 14:16:43 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -4,20 +4,24 @@ import numpy as np
 from matplotlib import image
 import os
 
-st.title("Restaurant Rating Prediction")
-st.subheader("The main goal of this is to get insights on restaurants which people like to visit and to identify the rating of the restaurants in area.")
-st.write("Restaurant Rating has become the most commonly used parameter for judging a restaurant for any individual.\
-          Rating of a restaurant depends on factors like reviews, area situated, average cost for two people, votes, \
-          cuisines and the type of restaurant.")
+st.title("DineScore")
+st.subheader("The primary objective is to gain insights into popular restaurants and determine their ratings.")
+st.write("Restaurant ratings are widely used to evaluate eateries by individuals. \
+         The rating of a restaurant is influenced by factors such as reviews, location, average cost for two, \
+         votes, cuisines, and restaurant type.")
 
-col1,col2=st.columns(2)
-with col1:
-    st.write("[Github Link](https://github.com/Namsrkive/Machine-Learning-Project.git)")
-with col2:
-    st.write("[Kaggle Link](https://www.kaggle.com/datasets/himanshupoddar/zomato-bangalore-restaurants)")
 
+ # Used to get the directory path of the current Python script file that is being executed.
 file_dir=os.path.dirname(os.path.abspath(__file__))
+
+# combines the folder path from the previous step with the folder name "resources" to create a new path.
 dir_of_interest = os.path.join(file_dir, "resources")
+
+# combines the path from the previous step with the image file name "image.jpg" to create a new path. 
 image_path= os.path.join(dir_of_interest, "image.jpg")
+
+# reads the image file located at the path obtained in the previous step and stores it in the variable img.
 img = image.imread(image_path)
+
+# displays the image stored in the img variable.
 st.image(img)
