7007affa7da2dbbac127fc390acb172a91e30df7
Namrata <124352728+Namsrkive@users.noreply.github.com>
2024-11-13 14:13:10 +0530

    Update Prediction.py

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -1,23 +1,29 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 import joblib
+from matplotlib import image
+import lightgbm
+import os
 
-# Load the pre-trained model
-def load_model():
-    return joblib.load('model.pkl')
-model = load_model()
+st.title("Restaurant Rating Prediction")
 
-import random
-# Function to make predictions()
-def make_prediction(location, cuisines, online_order, book_table, votes, approx_cost):
-    prediction = random.uniform(3, 5)
-    return prediction
+file_dir=os.path.dirname(os.path.abspath(__file__))
+parent_dir=os.path.join(file_dir,os.pardir)
+dir_of_interest=os.path.join(parent_dir,"artifacts")
 
-# Main function to run the app
-def main():
-    st.title("Restaurant Rating Predictor")
+model_path=os.path.join(dir_of_interest,"model.pkl")
+encoder_path=os.path.join(dir_of_interest,"encoder.pkl")
+mlb_path=os.path.join(dir_of_interest,"mlb.pkl")
 
-    # Widgets for user input
-    location=st.selectbox("City",('Banashankari', 'Bannerghatta Road', 'Basavanagudi', 'Bellandur',
+# The model, encoder, and mlb variables load the artifacts using the joblib.load function. The open function is used to open the file in binary mode ('rb').
+model = joblib.load(open(model_path, 'rb'))
+encoder = joblib.load(open(encoder_path, 'rb'))
+mlb = joblib.load(open(mlb_path, 'rb'))
+
+# This code sets up a form using st.form to gather user inputs for the restaurant rating prediction.
+with st.form('user_inputs'):
+    City=st.selectbox("City",('Banashankari', 'Bannerghatta Road', 'Basavanagudi', 'Bellandur',
                               'Brigade Road', 'Brookefield', 'BTM', 'Church Street',
                               'Electronic City', 'Frazer Town', 'HSR', 'Indiranagar',
                               'Jayanagar', 'JP Nagar', 'Kalyan Nagar', 'Kammanahalli',
@@ -26,26 +32,49 @@ def main():
                               'Malleshwaram', 'Marathahalli', 'MG Road', 'New BEL Road',
                               'Old Airport Road', 'Rajajinagar', 'Residency Road',
                               'Sarjapur Road', 'Whitefield'))
-    cuisines = st.multiselect("Cuisines", ['Afghani', 'African', 'American', 'Andhra', 'Arabian', 'Asian',
-                                                'Assamese', 'Awadhi', 'BBQ', 'Bakery', 'Belgian', 'Bengali', 'Beverages',
-                                                'Bihari', 'Biryani', 'Bohri', 'British', 'Burmese', 'Cantonese', 'Chettinad',
-                                                'Chinese', 'Continental', 'Desserts', 'European', 'Fast Food', 'French', 'German',
-                                                'Goan', 'Greek', 'Gujarati', 'Healthy Food', 'Hyderabadi', 'Indonesian', 'Iranian',
-                                                'Italian', 'Japanese', 'Jewish', 'Kashmiri', 'Kebab', 'Kerala', 'Konkan', 'Korean',
-                                                'Lebanese', 'Lucknowi', 'Maharashtrian', 'Malaysian', 'Mangalorean', 'Mediterranean',
-                                                'Mexican', 'Middle Eastern', 'Modern Indian', 'Mughlai', 'Naga', 'Nepalese', 'North Eastern',
-                                                'North Indian', 'Oriya', 'Parsi', 'Portuguese', 'Rajasthani', 'Russian', 'Seafood',
-                                                'Sindhi', 'Singaporean', 'South American', 'South Indian', 'Spanish', 'Sri Lankan',
-                                                'Tamil', 'Thai', 'Tibetan', 'Turkish', 'Vegan', 'Vietnamese'])
-    online_order = st.radio("Online Order Facility", ("Yes", "No"))
-    book_table = st.radio("Table Booking Facility",('Yes', 'No'))
-    votes = st.slider("Number of Votes", min_value=0, max_value=1000)
-    approx_cost = st.slider("Approx Cost for Two People", min_value=0.0, max_value=3000.0,value=100.0, step=10.0)
+    Cuisines=st.multiselect("Cuisines",('Afghani','African','American','Andhra','Arabian','Asian',
+                                        'Assamese','Awadhi','BBQ','Bakery','Belgian','Bengali','Beverages',
+                                        'Bihari','Biryani','Bohri','British','Burmese','Cantonese','Chettinad',
+                                        'Chinese','Continental','Desserts','European','Fast Food','French','German',
+                                        'Goan','Greek','Gujarati','Healthy Food','Hyderabadi','Indonesian','Iranian',
+                                        'Italian','Japanese','Jewish','Kashmiri','Kebab','Kerala','Konkan','Korean',
+                                        'Lebanese','Lucknowi','Maharashtrian','Malaysian','Mangalorean','Mediterranean',
+                                        'Mexican','Middle Eastern','Modern Indian','Mughlai','Naga','Nepalese','North Eastern',
+                                        'North Indian','Oriya','Parsi','Portuguese','Rajasthani','Russian','Seafood',
+                                        'Sindhi','Singaporean','South American','South Indian','Spanish','Sri Lankan',
+                                        'Tamil','Thai','Tibetan','Turkish','Vegan','Vietnamese'),
+                                        default=['North Indian','South Indian','Italian'])
+    Cost_Per_Person=st.slider("Cost_Per_Person", min_value=20.0, max_value=3000.0, value=100.0, step=10.0)
+    No_of_Best_Sellers=st.slider("No_of_Best_Sellers", min_value=0, max_value=10, value=3, step=1)
+    Delivery=st.radio("Online Ordering",(('Yes', 'No')))
+    Booking=st.radio("Table Booking ",('Yes', 'No'))
+    click=st.form_submit_button()
+
+if click:
     
+    # new DataFrame (df) is created with the user inputs (Delivery, Booking, and City).
+    df=pd.DataFrame([[Delivery,Booking,City]],columns=['Delivery', 'Booking', 'City'])
+    # encoder is used to transform the categorical features in df into one-hot encoded representation (one_hot_df)
+    one_hot_df=encoder.transform(df).toarray()
+    
+    # The number of varieties (No_of_Varieties) is calculated based on the selected cuisines.
+    No_of_Varieties=len(Cuisines)
+    # Numeric features (No_of_Best_Sellers, No_of_Varieties, and Cost_Per_Person) are combined into a NumPy array (numeric)
+    numeric=np.array([[No_of_Best_Sellers, No_of_Varieties, Cost_Per_Person]])
+    
+    # Another DataFrame (df) is created with the selected cuisines (Cuisines).
+    df=pd.DataFrame([[Cuisines]],columns=['Cuisines'])
+    # The mlb (multilabel binarizer) is used to transform the cuisine column in df into a binary matrix (mlb_df)
+    mlb_df=mlb.transform(df)
 
-    if st.button("Predict"):
-        prediction = make_prediction(location, cuisines, online_order, book_table, votes, approx_cost)
-        st.write(f"The predicted rating is: {prediction:.1f} / 5")
+    # All the feature arrays (numeric, mlb_df, and one_hot_df) are concatenated along the column axis to create the final input array (data)
+    data=np.concatenate([numeric,mlb_df,one_hot_df],axis=1)
+    # The loaded model (model) is used to predict the restaurant rating based on data.
+    rating=model.predict(data)
 
-if __name__ == '__main__':
-    main()
+    if rating>=4:
+        st.success(f"Restaurant Rating is: {round(rating[0],1)}")
+    elif rating>=3.5:
+        st.warning(f"Restaurant Rating is: {round(rating[0],1)}")
+    else:
+        st.error(f"Restaurant Rating is: {round(rating[0],1)}")
